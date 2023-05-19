c91e42830068a3852f56ca6da6b529586ea3f74c
Oleksii Anoshkin <anoshkinaleksey@gmail.com>
2023-05-19 15:01:47 +0300

    updated app

--- to-do-list/src/components/TheHiddenBtn.vue
+++ to-do-list/src/components/TheHiddenBtn.vue
@@ -4,6 +4,11 @@ import { reactive, ref } from 'vue'
 let hideCompleted = ref(true);
 let message = reactive({ text: "Show all" });
 
+const firstContainerClass = ref("navbar bg-body-tertiary");
+const secondContainerClass = ref("container-fluid");
+const thirdContainerClass = ref("d-flex");
+const btnClass = ref("btn btn-secondary");
+
 const props = defineProps({
   btnClass: String
 })
@@ -18,15 +23,13 @@ function response(prop) {
 </script>
 
 <template>
-  <button :class="btnClass" @click="response(hideCompleted)">
-    {{ message.text }}
-  </button>
+      <div :class="firstContainerClass">
+        <div :class="secondContainerClass">
+          <div :class="thirdContainerClass">
+            <button :class="btnClass" @click="response(hideCompleted)">
+              {{ message.text }}
+            </button>
+          </div>
+        </div>
+      </div>
 </template>
-
-<style>
-footer.btn-secondary {
-  margin-left: 4px;
-
-  width: 160px;
-}
-</style>
